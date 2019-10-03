02f67bf8ddcb82833ad99d1b4ad39844f2f94afd
Christine <christine.altmann@gmx.com>
2019-10-03 09:01:28 +0100

    add contact icon

--- src/components/mutual/navbar/NavBar.js
+++ src/components/mutual/navbar/NavBar.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./NavBar.css";
+import "../../landingpage/css/LandingPage.css";
 
 const NavBar = () => {
   return (
-    <section className="navbar">
-      <i></i>
-      <i></i>
-      <i></i>
-    </section>
+    <>
+      <section className="navbar">
+        <i></i>
+        <i></i>
+        <i></i>
+      </section>
+      <section className="contact">
+        <img
+          className="contact-icon"
+          src={require("../../../assets/contact.svg")}
+        />
+      </section>
+    </>
   );
 };
 
