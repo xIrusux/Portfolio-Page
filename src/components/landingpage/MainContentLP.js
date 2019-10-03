511e90a2ba2b91b20f0e78ca8064974e830481eb
Christine <christine.altmann@gmx.com>
2019-10-03 08:23:06 +0100

    alligned arrow

--- src/components/landingpage/MainContentLP.js
+++ src/components/landingpage/MainContentLP.js
@@ -3,24 +3,26 @@ import "./css/MainContentLP.css";
 
 const MainContentLP = () => {
   return (
-    <section className="main-content">
-      <img
-        className="main-content__imageellipse"
-        src={require("../../assets/ChristineJapan.png")}
-        alt="picture of Christine Altmann in the streets of tokio"
-      />
-      <p className="description">
-        Hey, My name is Christine Altmann and I am a full-stack web-developer.
-        My passion is to create and to write smart code.
-      </p>
-      <p className="call-me">You can also call me</p>
-      <p className="changemaker">changemaker</p>
+    <>
+      <section className="main-content">
+        <img
+          className="main-content__imageellipse"
+          src={require("../../assets/ChristineJapan.png")}
+          alt="picture of Christine Altmann in the streets of tokio"
+        />
+        <p className="description">
+          Hey, My name is Christine Altmann and I am a full-stack web-developer.
+          My passion is to create and to write smart code.
+        </p>
+        <p className="call-me">You can also call me</p>
+        <p className="changemaker">changemaker</p>
+      </section>
       <img
         className="arrow--down"
         src={require("../../assets/arrow.svg")}
         alt="dowwards arrow to move to project page"
       />
-    </section>
+    </>
   );
 };
 
