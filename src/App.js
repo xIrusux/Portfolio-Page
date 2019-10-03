f77c93542233e277732cf49fb790b13b9f4add29
Christine <christine.altmann@gmx.com>
2019-10-03 09:22:31 +0100

    setup page logic first step

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { LandingPage } from "./components/landingpage/LandingPage";
 import { ProjectPage } from "./components/projectpage/ProjectPage";
 
 function App() {
-  return <LandingPage />;
+  const [page, setPage] = React.useState("landingpage");
+  const [arrowdown, clickedArrowdown] = React.useState(false);
+  return (
+    <LandingPage arrowdown={arrowdown} clickedArrowdown={clickedArrowdown} />
+  );
 }
 
 export default App;
