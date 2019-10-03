6d83df0f6fb66c82362d1b931747d7815b92a4a7
Christine <christine.altmann@gmx.com>
2019-10-03 09:12:44 +0100

    complete initial css set up of nav bar

--- src/components/mutual/navbar/NavBar.js
+++ src/components/mutual/navbar/NavBar.js
@@ -3,6 +3,10 @@ import "./NavBar.css";
 import "../../landingpage/css/LandingPage.css";
 
 const NavBar = () => {
+  // on hover change state to class hover and increase margin between items
+
+  // add 2 additional states for transition into X
+
   return (
     <>
       <section className="navbar">
