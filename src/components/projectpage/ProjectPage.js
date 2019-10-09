c93d7a0663b9f1014ad78fe1bbe04ff12e47cc8d
Christine <christine.altmann@gmx.com>
2019-10-09 08:36:07 +0100

    base grid setup

--- src/components/projectpage/ProjectPage.js
+++ src/components/projectpage/ProjectPage.js
@@ -5,11 +5,28 @@ const ProjectPage = () => {
   return (
     <section className="project-page">
       <h2>Projects</h2>
+      <hr className="line" />
       <ul className="project-grid">
-        <li className="project-item"></li>
-        <li className="project-item"></li>
-        <li className="project-item"></li>
-        <li className="project-item"></li>
+        <li className="project-item">
+          <img src="./logo.png" alt="story"></img>
+          <h1>Project title</h1>
+          <p>category</p>
+        </li>
+        <li className="project-item">
+          <img src="./logo.png" alt="story"></img>
+          <h1>Project title</h1>
+          <p>category</p>
+        </li>
+        <li className="project-item">
+          <img src="./logo.png" alt="story"></img>
+          <h1>Project title</h1>
+          <p>category</p>
+        </li>
+        <li className="project-item">
+          <img src="./logo.png" alt="story"></img>
+          <h1>Project title</h1>
+          <p>category</p>
+        </li>
       </ul>
       <div className="projects__container"></div>
     </section>
