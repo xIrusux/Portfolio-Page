bf0394554eea44c3e08d16a2d9833c8625a0b6cc
Christine <christine.altmann@gmx.com>
2019-10-09 09:06:10 +0100

    add and align with navbar

--- src/components/projectpage/ProjectPage.js
+++ src/components/projectpage/ProjectPage.js
@@ -1,51 +1,55 @@
 import React from "react";
 import "./css/ProjectPage.css";
+import { NavBar } from "../mutual/navbar/NavBar";
 
 const ProjectPage = () => {
   return (
-    <section className="project-page">
-      <h2>Projects</h2>
-      <hr className="line" />
-      <div className="project-grid">
-        <article className="project-item">
-          <img
-            className="project-image"
-            src={require("../../assets/placeholder-image.png")}
-            alt="story"
-          ></img>
-          <h1>Project title</h1>
-          <p>category</p>
-        </article>
-        <article className="project-item">
-          <img
-            className="project-image"
-            src={require("../../assets/placeholder-image.png")}
-            alt="story"
-          ></img>
-          <h1>Project title</h1>
-          <p>category</p>
-        </article>
-        <article className="project-item">
-          <img
-            className="project-image"
-            src={require("../../assets/placeholder-image.png")}
-            alt="story"
-          ></img>
-          <h1>Project title</h1>
-          <p>category</p>
-        </article>
-        <article className="project-item">
-          <img
-            className="project-image"
-            src={require("../../assets/placeholder-image.png")}
-            alt="story"
-          ></img>
-          <h1>Project title</h1>
-          <p>category</p>
-        </article>
-      </div>
-      <div className="projects__container"></div>
-    </section>
+    <>
+      <NavBar />
+      <section className="project-page">
+        <h2>Projects</h2>
+        <hr className="line" />
+        <div className="project-grid">
+          <article className="project-item">
+            <img
+              className="project-image"
+              src={require("../../assets/placeholder-image.png")}
+              alt="story"
+            ></img>
+            <h1>Project title</h1>
+            <p>category</p>
+          </article>
+          <article className="project-item">
+            <img
+              className="project-image"
+              src={require("../../assets/placeholder-image.png")}
+              alt="story"
+            ></img>
+            <h1>Project title</h1>
+            <p>category</p>
+          </article>
+          <article className="project-item">
+            <img
+              className="project-image"
+              src={require("../../assets/placeholder-image.png")}
+              alt="story"
+            ></img>
+            <h1>Project title</h1>
+            <p>category</p>
+          </article>
+          <article className="project-item">
+            <img
+              className="project-image"
+              src={require("../../assets/placeholder-image.png")}
+              alt="story"
+            ></img>
+            <h1>Project title</h1>
+            <p>category</p>
+          </article>
+        </div>
+        <div className="projects__container"></div>
+      </section>
+    </>
   );
 };
 
