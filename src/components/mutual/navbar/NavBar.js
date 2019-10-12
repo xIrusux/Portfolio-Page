52247b00338231efcdf4d9167dccbacd171e92e2
Christine <christine.altmann@gmx.com>
2019-10-12 19:32:07 +0100

    navbar animation set up

--- src/components/mutual/navbar/NavBar.js
+++ src/components/mutual/navbar/NavBar.js
@@ -10,11 +10,33 @@ const NavBar = () => {
 
   return (
     <>
-      <section className="navbar">
-        <i></i>
-        <i></i>
-        <i></i>
-      </section>
+      <nav role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox" />
+
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <ul id="menu">
+            <a href="#">
+              <li>Home</li>
+            </a>
+            <a href="#">
+              <li>About</li>
+            </a>
+            <a href="#">
+              <li>Info</li>
+            </a>
+            <a href="#">
+              <li>Contact</li>
+            </a>
+            <a href="https://christinealtmann.com/" target="_blank">
+              <li>Show me more</li>
+            </a>
+          </ul>
+        </div>
+      </nav>
       <section className="contact">
         <Link to={{ pathname: "/contact-me" }}>
           <img
